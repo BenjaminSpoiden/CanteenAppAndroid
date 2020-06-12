b1ad9f02f8b81a4dc366ecaee21712c2981fd815
BenjaminSpoiden <56724799+BenjaminSpoiden@users.noreply.github.com>
2020-06-12 12:15:44 +0200

    New branch for implement the views of the menu

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/utils/ActivityUtils.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/utils/ActivityUtils.kt
@@ -1,13 +1,6 @@
 package be.technifutur.devmob9.projet_cantinapp_android.utils
-import android.app.Activity
-import android.content.Context
 import android.graphics.Rect
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.fragment.app.Fragment
 
 object ActivityUtils{
     fun switchLayoutAnimationKeyboard(constraintRoot: MotionLayout){
