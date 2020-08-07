c9e795cd53430dd4e46d69f3e3262a548dda6ae3
BenjaminSpoiden <56724799+BenjaminSpoiden@users.noreply.github.com>
2020-08-07 11:43:02 +0200

    switched fast adapter to multiview adapter and finished implementing a single item selection on the calendar rv

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/view/ui/fragments/HomeFragment.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/view/ui/fragments/HomeFragment.kt
@@ -1,13 +1,9 @@
 package be.technifutur.devmob9.projet_cantinapp_android.view.ui.fragments
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,25 +13,15 @@ import be.technifutur.devmob9.projet_cantinapp_android.databinding.FragmentHomeB
 import be.technifutur.devmob9.projet_cantinapp_android.interfaces.CalendarListener
 import be.technifutur.devmob9.projet_cantinapp_android.model.data.CalendarModel
 import be.technifutur.devmob9.projet_cantinapp_android.model.firebase.CalendarDayManager
-import be.technifutur.devmob9.projet_cantinapp_android.utils.cardState
-import be.technifutur.devmob9.projet_cantinapp_android.utils.colorSelection
-import be.technifutur.devmob9.projet_cantinapp_android.view.adapter.CalendarItem
+import be.technifutur.devmob9.projet_cantinapp_android.view.adapter.CalendarBinder
 import be.technifutur.devmob9.projet_cantinapp_android.viewmodel.HomeViewModel
 import be.technifutur.devmob9.projet_cantinapp_android.viewmodel.factory.HomeViewModelFactory
-import com.google.android.material.card.MaterialCardView
-import com.mikepenz.fastadapter.FastAdapter
-import com.mikepenz.fastadapter.ISelectionListener
-import com.mikepenz.fastadapter.adapters.ItemAdapter
-import com.mikepenz.fastadapter.listeners.ClickEventHook
-import com.mikepenz.fastadapter.select.SelectExtension
-import com.mikepenz.fastadapter.select.getSelectExtension
-import com.mikepenz.fastadapter.select.selectExtension
-import kotlinx.android.synthetic.main.calendar_item.*
-import kotlinx.android.synthetic.main.calendar_item.view.*
+import mva2.adapter.ListSection
+import mva2.adapter.MultiViewAdapter
+import mva2.adapter.util.Mode
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
-import kotlin.properties.Delegates
 
 class HomeFragment: BaseFragment(), KodeinAware, CalendarListener {
     companion object {
@@ -52,10 +38,9 @@ class HomeFragment: BaseFragment(), KodeinAware, CalendarListener {
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var calendarRecyclerView: RecyclerView
-    private val itemAdapter = ItemAdapter<CalendarItem>()
-    private val fastAdapter = FastAdapter.with(itemAdapter)
-    private var selectedPosition: Int = -1
-    private var oldSelectedPosition: Int = -1
+
+    private lateinit var multiViewAdapter: MultiViewAdapter
+    private lateinit var listSection: ListSection<CalendarModel>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
@@ -66,32 +51,29 @@ class HomeFragment: BaseFragment(), KodeinAware, CalendarListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        calendarRecyclerView = view.findViewById(R.id.calendar_recyclerview)
+    }
+
+    override fun onStart() {
+        super.onStart()
         manager.calendarListener = this
 
-        calendarRecyclerView = view.findViewById(R.id.calendar_recyclerview)
+        multiViewAdapter = MultiViewAdapter()
+        multiViewAdapter.registerItemBinders(CalendarBinder())
+        multiViewAdapter.setSelectionMode(Mode.SINGLE)
+
+        listSection =  ListSection()
+        multiViewAdapter.addSection(listSection)
+
         calendarRecyclerView.apply {
-            this.adapter = fastAdapter
+            this.adapter = multiViewAdapter
             this.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         }
-        fastAdapter.addEventHook(object : ClickEventHook<CalendarItem>() {
-            override fun onBind(viewHolder: RecyclerView.ViewHolder): View? {
-                return if(viewHolder is CalendarItem.CalendarViwHolder){
-                    viewHolder.calendarCard
-                }else {
-                    null
-                }
-            }
-            override fun onClick(v: View, position: Int, fastAdapter: FastAdapter<CalendarItem>, item: CalendarItem) {
-                v.calendar_card_view.setCardBackgroundColor(colorSelection("#DBF9D8"))
-                v.day_tv.setTextColor(colorSelection("#2F4858"))
-                v.day_number_tv.setTextColor(colorSelection("#2F4858"))
-            }
-        })
     }
 
     override fun onCalendarReceived(calendarModel: CalendarModel) {
-        itemAdapter.add(CalendarItem(calendarModel))
-        fastAdapter.notifyAdapterDataSetChanged()
+        listSection.add(calendarModel)
+        multiViewAdapter.notifyDataSetChanged()
     }
 
     override fun onDetach() {
