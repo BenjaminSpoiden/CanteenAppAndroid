1e933e412033fcdcd2e5eb3abddd65d020e2b7cc
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2020-08-09 21:51:41 +0200

    retrieved data from firebase for the menu section

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/viewmodel/HomeViewModel.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/viewmodel/HomeViewModel.kt
@@ -1,8 +1,13 @@
 package be.technifutur.devmob9.projet_cantinapp_android.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import be.technifutur.devmob9.projet_cantinapp_android.interfaces.DayListener
 import be.technifutur.devmob9.projet_cantinapp_android.model.repositories.UserRepository
+import be.technifutur.devmob9.projet_cantinapp_android.view.adapter.CalendarBinder
 
 class HomeViewModel(private val userRepository: UserRepository): ViewModel() {
 
+    var dayOfWeek = MutableLiveData<String>()
 }
