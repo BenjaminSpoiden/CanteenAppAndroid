4c6db7621d1f6db26695cfe3997b5023c0d57276
devtordu <bertieaux.raphael@gmail.com>
2020-08-10 12:08:46 +0200

    refactor recyclerview_allergie_info.xml to recyclerview_allergie_info_left_allignement.xml add recyclerview_allergie_info_right_allignement.xml adapt design for these two layout add item definition to AllergiesModel.kt modification fun setupMockAllergies() => itemAdapter.add(AllergiesItem(AllergiesModel(it))) modification Constants.kt => listOfAllergies

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/utils/Constants.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/utils/Constants.kt
@@ -1,6 +1,7 @@
 package be.technifutur.devmob9.projet_cantinapp_android.utils
 
 import be.technifutur.devmob9.projet_cantinapp_android.R
+import be.technifutur.devmob9.projet_cantinapp_android.model.data.AllergiesModel
 
 object Constants {
     const val FIREBASE_TAG = "FirebaseTest"
@@ -17,20 +18,20 @@ object Constants {
     const val EMAIL = "email"
     const val PASSWORD = "password"
 
-    val listOfAllergies = listOf(
-        R.drawable.ic_gluten,
-        R.drawable.ic_celery,
-        R.drawable.ic_almond,
-        R.drawable.ic_crustaceans,
-        R.drawable.ic_egg,
-        R.drawable.ic_fish,
-        R.drawable.ic_lupin,
-        R.drawable.ic_milk,
-        R.drawable.ic_mollusc,
-        R.drawable.ic_mustard,
-        R.drawable.ic_sesame,
-        R.drawable.ic_soybean,
-        R.drawable.ic_sulfide
+    val listOfAllergies = listOf<AllergiesModel>(
+        AllergiesModel(R.drawable.ic_gluten, "Céréales contenant du gluten", "le blé (comme l'épeautre et le blé de Khorasan), le seigle, l'orge, l'avoine ou les espèces hybrides."),
+        AllergiesModel(R.drawable.ic_celery, "Céleri", "Beurk"),
+        AllergiesModel(R.drawable.ic_almond, "Fruits à coque", "À savoir les amandes, les noisettes, les noix, les noix de cajou, les noix de pécan, les noix du Brésil, les noix de Macadamia et du Queensland et les pistaches. "),
+        AllergiesModel(R.drawable.ic_crustaceans, "Crustacés", "Beurk"),
+        AllergiesModel(R.drawable.ic_egg, "Oeuf", "Beurk"),
+        AllergiesModel(R.drawable.ic_fish, "Poissons", "Beurk"),
+        AllergiesModel(R.drawable.ic_lupin, "Lupin", "Beurk"),
+        AllergiesModel(R.drawable.ic_milk, "Lait", "Beurk"),
+        AllergiesModel(R.drawable.ic_mollusc, "Mollusques", "Beurk"),
+        AllergiesModel(R.drawable.ic_mustard, "Moutarde", "Beurk"),
+        AllergiesModel(R.drawable.ic_sesame, "Graines de sésame", "Beurk"),
+        AllergiesModel(R.drawable.ic_soybean, "Soja", "Beurk"),
+        AllergiesModel(R.drawable.ic_sulfide, "Anhydride sulfureux et sulfites", "Beurk")
     )
 
     val hashMapOfAllergies = hashMapOf(
