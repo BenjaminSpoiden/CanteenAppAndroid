d88fb760ef5b2705886be6ed488e58dbdd86ed25
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2020-08-11 23:19:12 +0200

    trying to figure out a way to implement the data

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/model/data/DishesType.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/model/data/DishesType.kt
@@ -1,17 +1,6 @@
 package be.technifutur.devmob9.projet_cantinapp_android.model.data
 
-import android.util.Log
-import be.technifutur.devmob9.projet_cantinapp_android.utils.Constants.FIREBASE_TAG
-
-
-enum class DishesTypes {
-    STARTERS,
-    MAIN_COURSES,
-    DESSERTS
-}
-
 sealed class DishesType: Food() {
-
     data class Starters(
         val name: String? = null,
         val description: String? = null,
@@ -19,25 +8,23 @@ sealed class DishesType: Food() {
         val energy: Int? = null,
         val sugar: Int? = null,
         val fat: Int? = null,
-        val protein: Int? = null
-    ): DishesType(){
-        fun showData() {
-            Log.d(FIREBASE_TAG, "data: name: $name, desc: $description, prix: $price, energie: $energy, sucre: $sugar, gras: $fat, proteins: $protein")
-        }
-    }
-    data class MainCourses(val name: String? = null,
-                           val description: String? = null,
-                           val price: Int? = null,
-                           val energy: Int? = null,
-                           val sugar: Int? = null,
-                           val fat: Int? = null,
-                           val protein: Int? = null): DishesType()
+        val protein: Int? = null): DishesType()
 
-    data class Desserts(val name: String? = null,
-                        val description: String? = null,
-                        val price: Int? = null,
-                        val energy: Int? = null,
-                        val sugar: Int? = null,
-                        val fat: Int? = null,
-                        val protein: Int? = null): DishesType()
+    data class MainCourses(
+        val name: String? = null,
+        val description: String? = null,
+        val price: Int? = null,
+        val energy: Int? = null,
+        val sugar: Int? = null,
+        val fat: Int? = null,
+        val protein: Int? = null): DishesType()
+
+    data class Desserts(
+        val name: String? = null,
+        val description: String? = null,
+        val price: Int? = null,
+        val energy: Int? = null,
+        val sugar: Int? = null,
+        val fat: Int? = null,
+        val protein: Int? = null): DishesType()
 }
