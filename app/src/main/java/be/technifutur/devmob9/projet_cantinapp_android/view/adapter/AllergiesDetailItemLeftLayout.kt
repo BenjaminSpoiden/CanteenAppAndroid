0c6b5f88e6719653f6ba8f347ffbea8bd721fd8f
Boulgras <bertieaux?raphael@gmail.com>
2020-08-14 22:52:17 +0200

    ajout du code dans les classes AllergiesDetailItemLeftLayout.kt et AllergiesDetailItemRightLayout.kt modification du feed de l'adapter dans la classe AllergiesDetailItem.kt pour alternance gauche/droite

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/view/adapter/AllergiesDetailItemLeftLayout.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/view/adapter/AllergiesDetailItemLeftLayout.kt
@@ -1,7 +1,11 @@
 package be.technifutur.devmob9.projet_cantinapp_android.view.adapter
 
+import be.technifutur.devmob9.projet_cantinapp_android.R
 import be.technifutur.devmob9.projet_cantinapp_android.model.data.AllergiesModel
 
 class AllergiesDetailItemLeftLayout(allergiesModel: AllergiesModel) : AllergiesDetailItem(allergiesModel) {
+
+    override val layoutRes: Int
+        get() = R.layout.recyclerview_allergie_info_left_allignement
 
 }
