60e9a2234d8ab3cd358b33edb7b9515f2b18f485
devtordu <bertieaux.raphael@gmail.com>
2020-08-18 12:29:24 +0200

    add data to listofallergene in Constants.kt

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/utils/Constants.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/utils/Constants.kt
@@ -20,19 +20,19 @@ object Constants {
 
     val listOfAllergies = listOf<AllergiesModel>(
         AllergiesModel(R.drawable.ic_gluten, "Céréales contenant du gluten", "le blé (comme l'épeautre et le blé de Khorasan), le seigle, l'orge, l'avoine ou les espèces hybrides."),
-        AllergiesModel(R.drawable.ic_celery, "Céleri", "Le céleri est une plante herbacée bisannuelle de la famille des Apiacées, cultivée comme plante potagère pour ses feuilles et sa racine tubérisée consommées comme légumes."),
+        AllergiesModel(R.drawable.ic_celery, "Céleri", "Plante herbacée bisannuelle de la famille des Apiacées, cultivée comme plante potagère pour ses feuilles et sa racine tubérisée consommées comme légumes."),
         AllergiesModel(R.drawable.ic_almond, "Fruits à coque", "À savoir les amandes, les noisettes, les noix, les noix de cajou, les noix de pécan, les noix du Brésil, les noix de Macadamia et du Queensland et les pistaches. "),
-        AllergiesModel(R.drawable.ic_crustaceans, "Crustacés", "Beurk"),
-        AllergiesModel(R.drawable.ic_egg, "Oeuf", "Beurk"),
-        AllergiesModel(R.drawable.ic_fish, "Poissons", "Beurk"),
-        AllergiesModel(R.drawable.ic_lupin, "Lupin", "Beurk"),
-        AllergiesModel(R.drawable.ic_milk, "Lait", "Beurk"),
-        AllergiesModel(R.drawable.ic_mollusc, "Mollusques", "Beurk"),
-        AllergiesModel(R.drawable.ic_mustard, "Moutarde", "Beurk"),
-        AllergiesModel(R.drawable.ic_sesame, "Graines de sésame", "Beurk"),
-        AllergiesModel(R.drawable.ic_soybean, "Soja", "Beurk"),
-        AllergiesModel(R.drawable.ic_sulfide, "Anhydride sulfureux et sulfites", "Beurk"),
-        AllergiesModel(R.drawable.ic_peanut, "Arachide", "Beurk")
+        AllergiesModel(R.drawable.ic_crustaceans, "Crustacés", "Invertébré arthropode, le plus souvent aquatique, ayant deux paires d'antennes, des yeux composés, un tégument chitineux plus ou moins imprégné de calcaire, une respiration branchiale, des anneaux (libres ou soudés) portant chacun une paire d'appendices dans la plupart des espèces."),
+        AllergiesModel(R.drawable.ic_egg, "Oeuf", "Cellule vivante, environnée de réserves alimentaires et d'enveloppes protectrices."),
+        AllergiesModel(R.drawable.ic_fish, "Poissons", "Animaux vertébrés aquatiques à branchies, pourvus de nageoires et dont le corps est le plus souvent couvert d'écailles."),
+        AllergiesModel(R.drawable.ic_lupin, "Lupin", "Plantes dicotylédones de la famille des Fabaceae (ou légumineuses), se caractérisent notamment par la richesse en protéines de leurs graines (jusqu'à 50 %)."),
+        AllergiesModel(R.drawable.ic_milk, "Lait", "Liquide biologique comestible généralement de couleur blanchâtre produit par les glandes mammaires des mammifères femelles."),
+        AllergiesModel(R.drawable.ic_mollusc, "Mollusques", "Animaux non segmentés, à symétrie bilatérale quelquefois altérée. Leur corps se compose généralement d'une tête, d'une masse viscérale, et d'un pied. La masse viscérale est recouverte en tout ou partie par un manteau, qui sécrète une coquille calcaire."),
+        AllergiesModel(R.drawable.ic_mustard, "Moutarde", "Condiment préparé à partir des graines d'une plante de la famille des Brassicaceae, appelée aussi moutarde."),
+        AllergiesModel(R.drawable.ic_sesame, "Graines de sésame", "Graines provenant du sésame (Sesamum indicum) qui est une plante de la famille des Pédaliacées."),
+        AllergiesModel(R.drawable.ic_soybean, "Soja", "La graine de soja est une graine oléoprotéagineuse et comestible. Produite par le soja , une plante annuelle originaire d’Asie orientale. Riche en protéines et en lipides, avec un profil en acides gras intéressant, cette graine qui est par ailleurs bien pourvue en vitamines du groupe B et en isoflavones, offre des atouts nutritionnels évidents."),
+        AllergiesModel(R.drawable.ic_sulfide, "Anhydride sulfureux et sulfites", "Les sulfites sont des substances naturellement présentes dans certains aliments. Ils sont également utilisés comme additifs pour préserver la couleur des aliments, prolonger leur durée de conservation et prévenir la croissance de champignons ou de bactéries."),
+        AllergiesModel(R.drawable.ic_peanut, "Arachide", "Plante dont le fruit comestible s'appelle cacahuète.")
     )
 
     val hashMapOfAllergies = hashMapOf(
