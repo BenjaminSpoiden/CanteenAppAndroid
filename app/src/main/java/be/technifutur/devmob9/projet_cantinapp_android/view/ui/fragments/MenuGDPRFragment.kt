44896e34df3cb62af67e9a4f78b80df9f08f17a0
devtordu <bertieaux.raphael@gmail.com>
2020-08-20 09:43:36 +0200

    Modification of MenuGDPRFragment.kt and fragment_menu_gdpr.xml to set up design and texts Add ressource file gdpr.txt to assets

--- app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/view/ui/fragments/MenuGDPRFragment.kt
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/view/ui/fragments/MenuGDPRFragment.kt
@@ -1,8 +1,11 @@
 package be.technifutur.devmob9.projet_cantinapp_android.view.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
 import be.technifutur.devmob9.projet_cantinapp_android.R
+import kotlinx.android.synthetic.main.fragment_menu_gdpr.*
+import kotlinx.android.synthetic.main.fragment_tos.*
 
 class MenuGDPRFragment: BaseFragment() {
     companion object {
@@ -22,5 +25,35 @@ class MenuGDPRFragment: BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        context?.let { fillConditionText(it) }
     }
+
+    fun fillConditionText (context: Context) {
+
+        val content = context.applicationContext?.assets?.open("gdpr.txt")?.bufferedReader()
+            .use { it?.readText() }
+        println(content)
+
+        val collWhat = content?.substringAfter("@What data do we collect?@")
+            ?.substringBefore("@How do we collect your data?@")
+
+        val collHow = content?.substringAfter("@How do we collect your data?@")
+            ?.substringBefore("@How will we use your data?@")
+
+        val useData =
+            content?.substringAfter("@How will we use your data?@")?.substringBefore("@Marketing@")
+
+        val market = content?.substringAfter("@Marketing@")
+            ?.substringBefore("@What are your data protection rights?@")
+
+        val dpr = content?.substringAfter("@What are your data protection rights?@")
+
+        collectWhatText.text = collWhat
+        collectHowText.text = collHow
+        dataUseText.text = useData
+        marketingText.text = market
+        dataProtectionRightsText.text = dpr
+    }
+
 }
