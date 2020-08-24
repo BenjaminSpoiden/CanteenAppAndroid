554ee21129c50a8904d9853ceb92277a92d727db
BenjaminSpoiden <56724799+BenjaminSpoiden@users.noreply.github.com>
2020-08-24 17:03:24 +0200

    added TODO for the Detail ViewModel

---
+++ app/src/main/java/be/technifutur/devmob9/projet_cantinapp_android/viewmodel/DetailViewModel.kt
@@ -0,0 +1,11 @@
+package be.technifutur.devmob9.projet_cantinapp_android.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel: ViewModel() {
+    /**
+     * TODO: Fonction qui permet de calculer la hauteur d'une des views selon les formules
+     * TODO: pour les differents macro-nutriments
+     * TODO: Renvoyer la function sous forme de liveData et l'observer dans le Fragment ?
+     */
+}
